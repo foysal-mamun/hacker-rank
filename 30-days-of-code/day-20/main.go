97b79c53da5430dbe51aa76832fbab612c080f2c
Foysal Mamun <sal_mdn@yahoo.com>
2016-06-24 18:00:41 +0600

    day 20 done

--- 30-days-of-code/day-20/main.go
+++ 30-days-of-code/day-20/main.go
@@ -12,5 +12,34 @@ func main() {
 		fmt.Scan(&a[i])
 	}
 
-	fmt.Println(&a)
+	a, numSwp := bubbleSwap(a, n)
+
+	fmt.Printf("Array is sorted in %v swaps.\n", numSwp)
+	fmt.Printf("First Element: %v\n", a[0])
+	fmt.Printf("Last Element: %v", a[n-1])
+}
+
+func bubbleSwap(a []int, n int) ([]int, int) {
+
+	numSwp := 0
+
+	for i := 0; i < n; i++ {
+
+		ns := 0
+
+		for j := 0; j < n-1; j++ {
+			if a[j] > a[j+1] {
+				a[j], a[j+1] = a[j+1], a[j]
+				ns++
+			}
+		}
+
+		numSwp += ns
+
+		if ns == 0 {
+			break
+		}
+	}
+
+	return a, numSwp
 }
